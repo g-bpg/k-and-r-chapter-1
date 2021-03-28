c3a4f9892222858f47d47bf4d213cab11fc5053c
saadiens <rainbowcanbeblack@gmail.com>
2021-03-28 01:32:21 +0100

    villayer's solution

---
+++ 1-01/villayer.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+main(){
+	printf("hello, world\c");
+}
+
+/*
+if you remove the first line the 
+program still runs but it will ask 
+you to include <stdio.h> or provide
+a declaration of printf()
+
+if you remove ; ( { "
+the program will not run and will 
+tell you there is something missing
+*/
